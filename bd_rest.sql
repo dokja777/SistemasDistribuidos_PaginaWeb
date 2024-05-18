ae6e9f5884d8050f0434d40079dba65e0ad5abb1
maria la madrid <128330171+dokja777@users.noreply.github.com>
2024-05-18 13:55:09 -0500

    Update bd_rest.sql

--- bd_rest.sql
+++ bd_rest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2024 a las 12:46:04
+-- Tiempo de generación: 18-05-2024 a las 20:43:03
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -35,29 +35,30 @@ CREATE TABLE `t_cliente` (
   `DNI` varchar(8) NOT NULL,
   `Telefono` varchar(10) DEFAULT NULL,
   `Movil` varchar(9) DEFAULT NULL,
-  `estado` varchar(11) NOT NULL
+  `estado` varchar(11) NOT NULL,
+  `En linea` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `t_cliente`
 --
 
-INSERT INTO `t_cliente` (`Id_Cliente`, `Apellidos`, `Nombres`, `Direccion`, `DNI`, `Telefono`, `Movil`, `estado`) VALUES
-('C00001', 'GARCIA ANDONAIRE', 'JAVIER DAVID', 'AV.LOS ALAMOS 1234', '12345678', '3211234', '987654321', 'ACTIVADO'),
-('C00002', 'TREJO', 'BARRIENTOS', 'AV.LOS ALAMOS N°456', '45657524', '982330012', '45657524', 'DESACTIVADO'),
-('C00003', 'ZAMUDIO', 'DE LA CRUZ', 'AV.BAYOVAR N°123', '10550273', '989046299', '43434343', ''),
-('C00004', 'IZAGUIRRE', 'HUILLCA', 'CL ALCANFORES N° 911', '87654321', '3211298', '957647208', ''),
-('C00005', 'ONTIVEROS', 'JAMES', 'LOS CABITOS', '98765432', '2587876', '999999999', ''),
-('C00006', 'CRISTOPHER', 'QUISPE', 'AV LO FLORESTA N°400', '76376451', '12345677', '968218168', ''),
-('C00007', 'PALMADERA', 'MANUEL', 'AV. Bolivar N°11', '12345678', '7521154', '964314264', ''),
-('C00008', 'LORENZO', 'WILDER', 'Av. elias aguirre nÂ° 100', '12344320', '999888777', '87654321', ''),
-('C00009', 'VARGAS', 'CHAVEZ', 'CALLE MANTER 420', '4250000', '73307686', '992878337', ''),
-('C00010', 'SANCHEZ', 'FERNANDEZ PRADA', 'AV DEL EJERCITO N° 1746 ', '41810916', '34554398', '988455739', ''),
-('C00012', 'GARCIA', 'SANCHEZ', 'AV.LOS ALAMOS 1222', '12211221', '2673421', '997665443', ''),
-('C00013', 'SANCHEZ', 'MEDRANO', 'AV.LOS ALAMOS NÂ°123', '80982312', '4327656', '998777123', ''),
-('C00014', 'URBINA', 'SALAS', 'AV.LA CALAMEÃ?A 321', '12121313', '2123438', '991232123', ''),
-('C00015', 'TERRONES', 'LILIA', 'AV.LOS HEROES 121', '12344567', '78908767', '995456323', ''),
-('C00016', 'QUESQUEN URIARTE', 'FRANCISCO', 'AV.LOS ALAMOS NÃ?Â°123', '80121315', '3245623', '999888765', '');
+INSERT INTO `t_cliente` (`Id_Cliente`, `Apellidos`, `Nombres`, `Direccion`, `DNI`, `Telefono`, `Movil`, `estado`, `En linea`) VALUES
+('C00001', 'GARCIA ANDONAIRE', 'JAVIER DAVID', 'AV.LOS ALAMOS 1234', '12345678', '3211234', '987654321', 'activo', 0),
+('C00002', 'TREJO', 'BARRIENTOS', 'AV.LOS ALAMOS N°456', '45657524', '982330012', '45657524', 'inactivo', 0),
+('C00003', 'ZAMUDIO', 'DE LA CRUZ', 'AV.BAYOVAR N°123', '10550273', '989046299', '43434343', '', 0),
+('C00004', 'IZAGUIRRE', 'HUILLCA', 'CL ALCANFORES N° 911', '87654321', '3211298', '957647208', '', 0),
+('C00005', 'ONTIVEROS', 'JAMES', 'LOS CABITOS', '98765432', '2587876', '999999999', '', 0),
+('C00006', 'CRISTOPHER', 'QUISPE', 'AV LO FLORESTA N°400', '76376451', '12345677', '968218168', '', 0),
+('C00007', 'PALMADERA', 'MANUEL', 'AV. Bolivar N°11', '12345678', '7521154', '964314264', '', 0),
+('C00008', 'LORENZO', 'WILDER', 'Av. elias aguirre nÂ° 100', '12344320', '999888777', '87654321', '', 0),
+('C00009', 'VARGAS', 'CHAVEZ', 'CALLE MANTER 420', '4250000', '73307686', '992878337', '', 0),
+('C00010', 'SANCHEZ', 'FERNANDEZ PRADA', 'AV DEL EJERCITO N° 1746 ', '41810916', '34554398', '988455739', '', 0),
+('C00012', 'GARCIA', 'SANCHEZ', 'AV.LOS ALAMOS 1222', '12211221', '2673421', '997665443', '', 0),
+('C00013', 'SANCHEZ', 'MEDRANO', 'AV.LOS ALAMOS NÂ°123', '80982312', '4327656', '998777123', '', 0),
+('C00014', 'URBINA', 'SALAS', 'AV.LA CALAMEÃ?A 321', '12121313', '2123438', '991232123', '', 0),
+('C00015', 'TERRONES', 'LILIA', 'AV.LOS HEROES 121', '12344567', '78908767', '995456323', '', 0),
+('C00016', 'QUESQUEN URIARTE', 'FRANCISCO', 'AV.LOS ALAMOS NÃ?Â°123', '80121315', '3245623', '999888765', '', 0);
 
 -- --------------------------------------------------------
 
@@ -142,18 +143,20 @@ INSERT INTO `t_producto` (`Id_Prod`, `Descripcion`, `costo`, `precio`, `cantidad
 
 CREATE TABLE `t_usuario` (
   `IdUsuario` varchar(50) NOT NULL,
-  `Passwd` varchar(50) NOT NULL
+  `Passwd` varchar(50) NOT NULL,
+  `En linea` tinyint(1) NOT NULL,
+  `Estado` varchar(10) NOT NULL DEFAULT 'activo'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Volcado de datos para la tabla `t_usuario`
 --
 
-INSERT INTO `t_usuario` (`IdUsuario`, `Passwd`) VALUES
-('JGARCIA', '12345678'),
-('JPEREZ', 'HolaMundo'),
-('123', '123'),
-('Daniel', '123');
+INSERT INTO `t_usuario` (`IdUsuario`, `Passwd`, `En linea`, `Estado`) VALUES
+('AJGARCIA', '12345678', 0, 'activo'),
+('UJPEREZ', 'HolaMundo', 0, 'activo'),
+('U123', '123', 0, 'inactivo'),
+('ADaniel', '123', 0, 'activo');
 
 --
 -- Índices para tablas volcadas
